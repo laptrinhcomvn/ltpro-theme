386fdf5a79668ca4d2851930601224e2fdfefbc6
Tung Le <tung@laptrinh.com.vn>
2019-10-28 14:52:29 +0700

    fix error

--- functions.php
+++ functions.php
@@ -13,18 +13,9 @@ function ltpro_custom_logo_setup() {
 }
 add_action( 'after_setup_theme', 'ltpro_custom_logo_setup' );
 
-function ltpro_get_custom_logo() {
-    $custom_logo_id = get_theme_mod( 'custom_logo' );
-    $logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
-    if ( has_custom_logo() ) {
-        return esc_url( $logo[0] );
-    }
-    return '';
-}
-
 function ltpro_theme_widgets_init() {
     register_sidebar( array(
-        'name'          => __( 'Top Most Bar', 'ltmclassvn' ),
+        'name'          => __( 'Top Most Bar', 'laptrinhcomvn' ),
         'id'            => 'topbar',
         'description' => 'Please drop widget here to display it in Top Most Bar',
         'before_widget' => '<section id="%1$s" class="widget %2$s">',
@@ -34,9 +25,9 @@ function ltpro_theme_widgets_init() {
     ) );
 
     register_sidebar(array(
-        'name'          => __('Footer column 1', 'ltmclassvn'),
+        'name'          => __('Footer column 1', 'laptrinhcomvn'),
         'id'            => 'footer-colum1',
-        'description'   => __('Footer column 1 wigets.', 'ltmclassvn'),
+        'description'   => __('Footer column 1 wigets.', 'laptrinhcomvn'),
         'before_widget' => '<section id="%1$s" class="widget %2$s">',
         'after_widget'  => '</section>',
         'before_title'  => '<h5 class="widget-title">',
@@ -44,9 +35,9 @@ function ltpro_theme_widgets_init() {
     ));
 
     register_sidebar(array(
-        'name'          => __('Footer column 2', 'ltmclassvn'),
+        'name'          => __('Footer column 2', 'laptrinhcomvn'),
         'id'            => 'footer-colum2',
-        'description'   => __('Footer column 2 wigets.', 'ltmclassvn'),
+        'description'   => __('Footer column 2 wigets.', 'laptrinhcomvn'),
         'before_widget' => '<section id="%1$s" class="widget %2$s">',
         'after_widget'  => '</section>',
         'before_title'  => '<h5 class="widget-title">',
@@ -54,9 +45,9 @@ function ltpro_theme_widgets_init() {
     ));
 
     register_sidebar(array(
-        'name'          => __('Footer column 3', 'ltmclassvn'),
+        'name'          => __('Footer column 3', 'laptrinhcomvn'),
         'id'            => 'footer-colum3',
-        'description'   => __('Footer column 3 wigets.', 'ltmclassvn'),
+        'description'   => __('Footer column 3 wigets.', 'laptrinhcomvn'),
         'before_widget' => '<section id="%1$s" class="widget %2$s">',
         'after_widget'  => '</section>',
         'before_title'  => '<h5 class="widget-title">',
@@ -64,9 +55,9 @@ function ltpro_theme_widgets_init() {
     ));
     
     register_sidebar(array(
-        'name'          => __('Footer column 4', 'ltmclassvn'),
+        'name'          => __('Footer column 4', 'laptrinhcomvn'),
         'id'            => 'footer-colum4',
-        'description'   => __('Footer column 4 wigets.', 'ltmclassvn'),
+        'description'   => __('Footer column 4 wigets.', 'laptrinhcomvn'),
         'before_widget' => '<section id="%1$s" class="widget %2$s">',
         'after_widget'  => '</section>',
         'before_title'  => '<h5 class="widget-title">',
@@ -74,9 +65,9 @@ function ltpro_theme_widgets_init() {
     ));
 
     register_sidebar(array(
-        'name'          => __('bottom-bar', 'ltmclassvn'),
+        'name'          => __('bottom-bar', 'laptrinhcomvn'),
         'id'            => 'bottom-bar',
-        'description'   => __('Footer bottom-bar wigets.', 'ltmclassvn'),
+        'description'   => __('Footer bottom-bar wigets.', 'laptrinhcomvn'),
         'before_widget' => '<section id="%1$s" class="widget %2$s">',
         'after_widget'  => '</section>',
         'before_title'  => '<h5 class="widget-title">',
@@ -84,13 +75,13 @@ function ltpro_theme_widgets_init() {
     ));
 
     register_sidebar(array(
-        'name'          => __('right-side-bar', 'ltmclassvn'),
+        'name'          => __('right-side-bar', 'laptrinhcomvn'),
         'id'            => 'right-side-bar',
-        'description'   => __('Right side bar', 'ltmclassvn'),
+        'description'   => __('Right side bar', 'laptrinhcomvn'),
         'before_widget' => '<section id="%1$s" class="widget %2$s">',
         'after_widget'  => '</section>',
         'before_title'  => '<h5 class="widget-title">',
         'after_title'   => '</h5>',
     ));
 }
-add_action( 'widgets_init', 'ltmclassvn_theme_widgets_init' );
+add_action( 'widgets_init', 'ltpro_theme_widgets_init' );
