1f4d5489feb637d0db0fd941be71605f69e379e2
Tung Le <tung@laptrinh.com.vn>
2019-11-26 23:19:11 +0700

    Hỗ trợ animation khi nhấn đóng mở menu mobile

--- inc/main-nav.php
+++ inc/main-nav.php
@@ -9,15 +9,15 @@
         </div>
 
         <div class="col-2 col-sm-1 d-block d-md-none">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-nav-mobile" aria-controls="main-nav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-nav-mobile" aria-controls="main-nav-mobile" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="sr-only">Toggle navigation</span>
                 <i class="fa fa-bars"></i>
             </button>
         </div>
 
-        <div class="col-0 col-sm-0 col-md-8 d-none d-md-block">
+        <div class="col-0 col-sm-0 col-md-8 collapse d-md-block">
             <div class="lg-nav">
-                <div class="collapse navbar-collapse d-flex justify-content-center" id="main-nav">
+                <div class="d-flex justify-content-center">
                     <?php
                     wp_nav_menu( array(
                         'theme_location' => 'primary',
@@ -40,19 +40,18 @@
 </div>
 
 
-<div class="animated fadeInDown faster container-fluid d-none w-100 h-100 nav-mobile" id="main-nav-mobile">
+<div class="container-fluid w-100 h-100 nav-mobile animated faster collapse" id="main-nav-mobile">
     <div class="text-right">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-nav-mobile" aria-controls="main-nav" aria-expanded="true" aria-label="Toggle navigation">
-                <span class="sr-only">Toggle navigation</span>
-                <i class="fas fa-times"></i>
-            </button>
+        <button class="navbar-toggler close" type="button" data-toggle="collapse" aria-controls="main-nav-mobile" data-target="#main-nav-mobile" aria-expanded="true" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
     </div>
 
     <?php
     wp_nav_menu( array(
         'theme_location' => 'primary',
-        'menu_id'        => 'primary-menu',
-        'menu_class'     => 'navbar-nav mr-auto mobile',
+        'menu_id'        => 'primary-mobile-nav',
+        'menu_class'     => 'navbar-nav mobile',
         'container_id'   => 'primary-top-menu-mobile'
     ) );
     ?>
--- js/ltpro.js
+++ js/ltpro.js
@@ -11,3 +11,23 @@ WebFont.load({
         document.documentElement.classList.add('wf-active');
     }
 })();
+
+jQuery(function(){
+    const mainMobileNav = jQuery('#main-nav-mobile');
+    mainMobileNav.on('show.bs.collapse', function () {
+        mainMobileNav.addClass('fadeInLeft');
+    });
+    mainMobileNav.on('shown.bs.collapse', function () {
+        mainMobileNav.removeClass('fadeInLeft');
+    });
+    mainMobileNav.on('hide.bs.collapse', function () {
+        mainMobileNav.addClass('fadeOutLeft');
+    });
+    mainMobileNav.on('hidden.bs.collapse', function () {
+        mainMobileNav.removeClass('fadeOutLeft');
+    });
+    mainMobileNav.on("click", "a", null, function () {
+        mainMobileNav.collapse('hide');
+    });
+});
+
